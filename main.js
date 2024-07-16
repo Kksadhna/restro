a7b7ab2398c1ad2e592af566eb8679d1095750d8
Kksadhna <ritikaparashar21194@gmail.com>
2024-07-16 22:09:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// menu
+let menu=document.querySelector('.menu-icon');
+let navbar=document.querySelector('.navbar');
+menu.onclick =()=>{
+    menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu');
+}
+// close menu on scroll
+window.onscroll=()=>{
+    menu.classList.remove('move');
+    navbar.classList.remove('open-menu');
+}
+// scroll reveal
+const animate= ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:'2500',
+    delay:'400'
+});
+animate.reveal('.home-text',{origin:"left"});
+animate.reveal('.home-img',{origin:"bottom"});
+animate.reveal('.heading, .newsletter h2',{origin:"top"});
+animate.reveal("header,.feature-box,.feature-menu-box,.item-box,.m-item-box,.t-box, .newsletter ",{interval:100});
